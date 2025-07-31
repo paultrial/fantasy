8be430a9ded3bc3d42734d240888051ab2ff5ea6
Paul Olteanu <p.olteanu@rosoftlab.net>
2025-07-31 14:14:45 +0300

    moar data

---
+++ src/assets/Elite Men's Qualifying Start List for the Leogang World Cup DH 2025.js
@@ -0,0 +1,221 @@
+[{
+                    "columns": [new Date(-2208942000000), "Danny HART", "NORCO RACE DIVISION", "GBR", new Date(662688000000)]
+                }, {
+                    "columns": [new Date(-2208941970000), "Oliver ZWAR", "ORBEA / FMD RACING", "SWE", new Date(788918400000)]
+                }, {
+                    "columns": [new Date(-2208941940000), "Luke MEIER-SMITH", "GIANT FACTORY OFF-ROAD TEAM - DH", "AUS", new Date(1009843200000)]
+                }, {
+                    "columns": [new Date(-2208941910000), "Simon CHAPELET", "ROGUE RACING - SR SUNTOUR", "FRA", new Date(1009843200000)]
+                }, {
+                    "columns": [new Date(-2208941880000), "Finn ILES", "SPECIALIZED GRAVITY", "CAN", new Date(915148800000)]
+                }, {
+                    "columns": [new Date(-2208941850000), "Andreas KOLB", "YT MOB", "AUT", new Date(820454400000)]
+                }, {
+                    "columns": [new Date(-2208941820000), "Benoit COULANGES", "SCOTT DOWNHILL FACTORY", "FRA", new Date(757382400000)]
+                }, {
+                    "columns": [new Date(-2208941790000), "Joe BREEDEN", "AXESS INTENSE FACTORY RACING", "GBR", new Date(915148800000)]
+                }, {
+                    "columns": [new Date(-2208941760000), "Antoine PIERRON", "COMMENCAL SCHWALBE BY LES ORRES", "FRA", new Date(915148800000)]
+                }, {
+                    "columns": [new Date(-2208941730000), "Richard RUDE JR", "YETI / FOX FACTORY RACE TEAM", "USA", new Date(788918400000)]
+                }, {
+                    "columns": [new Date(-2208941700000), "Thibaut DAPRELA", "ROGUE RACING - SR SUNTOUR", "FRA", new Date(978307200000)]
+                }, {
+                    "columns": [new Date(-2208941670000), "Ryan PINKERTON", "MONDRAKER FACTORY RACING DH", "USA", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208941640000), "Jordan WILLIAMS", "SPECIALIZED GRAVITY", "GBR", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208941610000), "Nathan PONTVIANNE", "GOODMAN SANTACRUZ", "FRA", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208941580000), "Loris VERGIER", "COMMENCAL/MUC-OFF BY RIDING ADDICTION", "FRA", new Date(820454400000)]
+                }, {
+                    "columns": [new Date(-2208941550000), "Luca SHAW", "CANYON CLLCTV FACTORY TEAM", "USA", new Date(820454400000)]
+                }, {
+                    "columns": [new Date(-2208941520000), "Oisin O'CALLAGHAN", "YT MOB", "IRL", new Date(1041379200000)]
+                }, {
+                    "columns": [new Date(-2208941490000), "Jackson GOLDSTONE", "SANTA CRUZ SYNDICATE", "CAN", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208941460000), "Loic BRUNI", "SPECIALIZED GRAVITY", "FRA", new Date(757382400000)]
+                }, {
+                    "columns": [new Date(-2208941430000), "Amaury PIERRON", "COMMENCAL/MUC-OFF BY RIDING ADDICTION", "FRA", new Date(820454400000)]
+                }, {
+                    "columns": [new Date(-2208941400000), "Lachlan STEVENS-MCNAB", "TREK FACTORY RACING DH", "NZL", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208941370000), "Rémi THIRION", "GIANT FACTORY OFF-ROAD TEAM - DH", "FRA", new Date(631152000000)]
+                }, {
+                    "columns": [new Date(-2208941340000), "Ronan DUNNE", "MONDRAKER FACTORY RACING DH", "IRL", new Date(1009843200000)]
+                }, {
+                    "columns": [new Date(-2208941310000), "Troy BROSNAN", "CANYON CLLCTV FACTORY TEAM", "AUS", new Date(725846400000)]
+                }, {
+                    "columns": [new Date(-2208941280000), "Charlie HATTON", "CONTINENTAL ATHERTON", "GBR", new Date(883612800000)]
+                }, {
+                    "columns": [new Date(-2208941250000), "Matt WALKER", "TREK FACTORY RACING DH", "GBR", new Date(915148800000)]
+                }, {
+                    "columns": [new Date(-2208941220000), "Dakotah NORTON", "MONDRAKER FACTORY RACING DH", "USA", new Date(694224000000)]
+                }, {
+                    "columns": [new Date(-2208941190000), "Max HARTENSTERN", "CUBE FACTORY RACING", "GER", new Date(915148800000)]
+                }, {
+                    "columns": [new Date(-2208941160000), "Thomas ESTAQUE", "GOODMAN SANTACRUZ", "FRA", new Date(820454400000)]
+                }, {
+                    "columns": [new Date(-2208941130000), "Jakob JEWETT", "GIANT FACTORY OFF-ROAD TEAM - DH", "CAN", new Date(1041379200000)]
+                }, {
+                    "columns": [new Date(-2208941100000), "Davide PALAZZARI", "ROGUE RACING - SR SUNTOUR", "ITA", new Date(788918400000)]
+                }, {
+                    "columns": [new Date(-2208941070000), "Laurie GREENLAND", "SANTA CRUZ SYNDICATE", "GBR", new Date(852076800000)]
+                }, {
+                    "columns": [new Date(-2208941040000), "Luke WAYMAN", "CONTINENTAL ATHERTON", "NZL", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208941010000), "Stefano INTROZZI", "", "ITA", new Date(946684800000)]
+                }, {
+                    "columns": [new Date(-2208940980000), "Tuhoto-Ariki PENE", "MS-RACING", "NZL", new Date(978307200000)]
+                }, {
+                    "columns": [new Date(-2208940950000), "Angel SUAREZ ALONSO", "FRAMEWORKS RACING / 5DEV", "ESP", new Date(788918400000)]
+                }, {
+                    "columns": [new Date(-2208940920000), "Oliver DAVIS", "TREK FACTORY RACING DH", "AUS", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208940890000), "Jackson CONNELLY", "THE ALLIANCE", "AUS", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208940860000), "Aaron GWIN", "GWIN RACING", "USA", new Date(536457600000)]
+                }, {
+                    "columns": [new Date(-2208940830000), "Roger VIEIRA", "", "BRA", new Date(757382400000)]
+                }, {
+                    "columns": [new Date(-2208940800000), "Reece WILSON", "AON RACING - TOURNE CAMPERVANS", "GBR", new Date(820454400000)]
+                }, {
+                    "columns": [new Date(-2208940770000), "Loris REVELLI", "", "ITA", new Date(852076800000)]
+                }, {
+                    "columns": [new Date(-2208940740000), "Dylan MAPLES", "COMMENCAL/MUC-OFF BY RIDING ADDICTION", "USA", new Date(1041379200000)]
+                }, {
+                    "columns": [new Date(-2208940710000), "Christian HAUSER", "TREK FACTORY RACING DH", "ITA", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208940680000), "Léo GRISEL", "", "FRA", new Date(1009843200000)]
+                }, {
+                    "columns": [new Date(-2208940650000), "Henri KIEFER", "CANYON CLLCTV FACTORY TEAM", "GER", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208940620000), "Daniel CASTELLANOS LIBERAL", "", "ESP", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208940590000), "James MACDERMID", "THE ALLIANCE", "NZL", new Date(1041379200000)]
+                }, {
+                    "columns": [new Date(-2208940560000), "Sebastian HOLGUIN VILLA", "MS-RACING", "COL", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208940530000), "Mylann FALQUET", "GOODMAN SANTACRUZ", "FRA", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208940500000), "Dom PLATT", "", "GBR", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208940470000), "Jack PIERCY", "COMMENCAL SCHWALBE BY LES ORRES", "GBR", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208940440000), "Gonçalo BANDEIRA", "SCOTT DOWNHILL FACTORY", "POR", new Date(1009843200000)]
+                }, {
+                    "columns": [new Date(-2208940410000), "George Ethan CRAIK", "SCOTT DOWNHILL FACTORY", "GBR", new Date(1041379200000)]
+                }, {
+                    "columns": [new Date(-2208940380000), "Mark WALLACE", "", "CAN", new Date(788918400000)]
+                }, {
+                    "columns": [new Date(-2208940350000), "Carter SLOAN", "", "AUS", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208940320000), "Julian STEINER", "", "GER", new Date(757382400000)]
+                }, {
+                    "columns": [new Date(-2208940290000), "Dane JEWETT", "GIANT FACTORY OFF-ROAD TEAM - DH", "CAN", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208940260000), "Davide CAPPELLO", "ROGUE RACING - SR SUNTOUR", "ITA", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208940230000), "Douglas VIEIRA", "", "BRA", new Date(883612800000)]
+                }, {
+                    "columns": [new Date(-2208940200000), "Martin MAES", "ORBEA / FMD RACING", "BEL", new Date(852076800000)]
+                }, {
+                    "columns": [new Date(-2208940170000), "Kye A'HERN", "KENDA NS BIKES UR TEAM", "AUS", new Date(978307200000)]
+                }, {
+                    "columns": [new Date(-2208940140000), "Sam GALE", "", "NZL", new Date(978307200000)]
+                }, {
+                    "columns": [new Date(-2208940110000), "Greg WILLIAMSON", "MONDRAKER FACTORY RACING DH", "GBR", new Date(694224000000)]
+                }, {
+                    "columns": [new Date(-2208940080000), "Pedro BURNS CONTRERAS", "", "CHI", new Date(852076800000)]
+                }, {
+                    "columns": [new Date(-2208940050000), "Baptiste PIERRON", "", "FRA", new Date(725846400000)]
+                }, {
+                    "columns": [new Date(-2208940020000), "Henry KERR", "AON RACING - TOURNE CAMPERVANS", "IRL", new Date(946684800000)]
+                }, {
+                    "columns": [new Date(-2208939990000), "Rory KIRK", "", "RSA", new Date(1041379200000)]
+                }, {
+                    "columns": [new Date(-2208939960000), "Hannes LEHMANN", "YT RACING DEVELOPMENT", "GER", new Date(978307200000)]
+                }, {
+                    "columns": [new Date(-2208939930000), "Antoine ROGGE", "", "FRA", new Date(1009843200000)]
+                }, {
+                    "columns": [new Date(-2208939900000), "Jacob DICKSON", "MS-RACING", "IRL", new Date(852076800000)]
+                }, {
+                    "columns": [new Date(-2208939870000), "Toby MEEK", "", "NZL", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208939840000), "Remy MEIER-SMITH", "PIVOT FACTORY RACING", "AUS", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208939810000), "Evan MEDCALF", "", "USA", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208939780000), "Jan LANER", "", "ITA", new Date(1009843200000)]
+                }, {
+                    "columns": [new Date(-2208939750000), "Ryan GRIFFITH", "PIVOT FACTORY RACING", "CAN", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208939720000), "Tilen LEBAN", "", "SLO", new Date(788918400000)]
+                }, {
+                    "columns": [new Date(-2208939690000), "Luca THURLOW", "", "GBR", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208939660000), "Alden PATE", "", "USA", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208939630000), "Noel NIEDERBERGER", "", "SUI", new Date(788918400000)]
+                }, {
+                    "columns": [new Date(-2208939600000), "Johan GARCIN", "", "FRA", new Date(1009843200000)]
+                }, {
+                    "columns": [new Date(-2208939570000), "Oscar GRIFFITHS", "AXESS INTENSE FACTORY RACING", "GBR", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208939540000), "Matheus Braian NEITZKE", "", "BRA", new Date(915148800000)]
+                }, {
+                    "columns": [new Date(-2208939510000), "Albertas TURBA", "", "LTU", new Date(757382400000)]
+                }, {
+                    "columns": [new Date(-2208939480000), "Alexandre FAYOLLE", "KENDA NS BIKES UR TEAM", "FRA", new Date(788918400000)]
+                }, {
+                    "columns": [new Date(-2208939450000), "Dylan LEVESQUE", "SCOTT DOWNHILL FACTORY", "FRA", new Date(820454400000)]
+                }, {
+                    "columns": [new Date(-2208939420000), "Gavin TOMLINSON", "GWIN RACING", "USA", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208939390000), "Sebastian WEINHANDL", "", "AUT", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208939360000), "Michael DELESALLE", "GWIN RACING", "CAN", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208939330000), "Roee OSTFELD", "", "ISR", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208939300000), "Jules TROILLET", "", "SUI", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208939270000), "Tyler ERVIN", "", "USA", new Date(1009843200000)]
+                }, {
+                    "columns": [new Date(-2208939240000), "Taylor VERNON", "", "GBR", new Date(820454400000)]
+                }, {
+                    "columns": [new Date(-2208939210000), "Fernando Juan MUÑOZ", "AXESS INTENSE FACTORY RACING", "COL", new Date(1041379200000)]
+                }, {
+                    "columns": [new Date(-2208939180000), "Simon MAURER", "", "GER", new Date(946684800000)]
+                }, {
+                    "columns": [new Date(-2208939150000), "Nico LAMM", "", "GER", new Date(915148800000)]
+                }, {
+                    "columns": [new Date(-2208939120000), "Noah HOFMANN", "", "AUT", new Date(1041379200000)]
+                }, {
+                    "columns": [new Date(-2208939090000), "Callum CROFTS", "", "AUS", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208939060000), "Neo TIELENS", "", "BEL", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208939030000), "Lucas LAGNEAU", "MS-RACING", "FRA", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208939000000), "Angus FERGUSON", "", "NZL", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208938970000), "Jan PAJAK", "", "POL", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208938940000), "Jorge GUARELLO ALONSO", "", "CHI", new Date(915148800000)]
+                }, {
+                    "columns": [new Date(-2208938910000), "Marius PERRAUDIN", "", "SUI", new Date(1009843200000)]
+                }, {
+                    "columns": [new Date(-2208938880000), "Eliel MAUKONEN", "", "FIN", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208938850000), "Christoph HANDL", "", "AUT", new Date(1041379200000)]
+                }, {
+                    "columns": [new Date(-2208938820000), "Guillermo TOVAR GALLEGO", "", "ESP", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208938790000), "Lucas CRUZ", "NORCO RACE DIVISION", "CAN", new Date(978307200000)]
+                }, {
+                    "columns": [new Date(-2208938760000), "Lucio VELLUTINO MALAGA", "", "PER", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208938730000), "Mikolaj GAWRONEK", "", "POL", new Date(1136073600000)]
+                }]
