3e4a3858e92fe146a452c6c834e734c32211e1ac
p.olteanu <p.olteanu@rosoftlab.net>
2025-09-03 15:20:31 +0300

    new data

---
+++ src/assets/quali/Elite Women's Qualifying Start List for the Les Gets World Cup DH 2025.js
@@ -0,0 +1,69 @@
+[{
+                    "columns": [new Date(-2208942900000), "GALE Phoebe", "ORBEA / FMD RACING", "GBR", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208942870000), "HRASTNIK Monika", "AON RACING - TOURNE CAMPERVANS", "SLO", new Date(757382400000)]
+                }, {
+                    "columns": [new Date(-2208942840000), "BLEWITT Jess", "CUBE FACTORY RACING", "NZL", new Date(1009843200000)]
+                }, {
+                    "columns": [new Date(-2208942810000), "NEWKIRK Anna", "FRAMEWORKS RACING / 5DEV", "USA", new Date(978307200000)]
+                }, {
+                    "columns": [new Date(-2208942780000), "CABIROU Marine", "CANYON CLLCTV FACTORY TEAM", "FRA", new Date(852076800000)]
+                }, {
+                    "columns": [new Date(-2208942750000), "HOFFMANN Nina", "SANTA CRUZ SYNDICATE", "GER", new Date(820454400000)]
+                }, {
+                    "columns": [new Date(-2208942720000), "BALANCHE Camille", "YETI / FOX FACTORY RACE TEAM", "SUI", new Date(631152000000)]
+                }, {
+                    "columns": [new Date(-2208942690000), "SEAGRAVE Tahnee", "ORBEA / FMD RACING", "GBR", new Date(788918400000)]
+                }, {
+                    "columns": [new Date(-2208942660000), "HEMSTREET Gracey", "NORCO RACE DIVISION", "CAN", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208942630000), "HÖLL Valentina", "YT MOB", "AUT", new Date(978307200000)]
+                }, {
+                    "columns": [new Date(-2208942600000), "BAUMANN Lisa", "COMMENCAL SCHWALBE BY LES ORRES", "SUI", new Date(978307200000)]
+                }, {
+                    "columns": [new Date(-2208942570000), "NICOLE Myriam", "COMMENCAL/MUC-OFF BY RIDING ADDICTION", "FRA", new Date(631152000000)]
+                }, {
+                    "columns": [new Date(-2208942540000), "WIDMANN Veronika", "MONDRAKER FACTORY RACING DH", "ITA", new Date(725846400000)]
+                }, {
+                    "columns": [new Date(-2208942510000), "JOHNSET Mille", "AXESS INTENSE FACTORY RACING", "NOR", new Date(978307200000)]
+                }, {
+                    "columns": [new Date(-2208942480000), "FERGUSON Louise-Anna", "AXESS INTENSE FACTORY RACING", "GBR", new Date(820454400000)]
+                }, {
+                    "columns": [new Date(-2208942450000), "SCARSI Gloria", "MS-RACING", "ITA", new Date(946684800000)]
+                }, {
+                    "columns": [new Date(-2208942420000), "BOULADOU Lisa", "GOODMAN SANTACRUZ", "FRA", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208942390000), "EARNEST Sacha", "TREK FACTORY RACING DH", "NZL", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208942360000), "HASTINGS Jenna", "PIVOT FACTORY RACING", "NZL", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208942330000), "VAN DER VELDEN Siel", "", "BEL", new Date(1041379200000)]
+                }, {
+                    "columns": [new Date(-2208942300000), "KHAQIMAH Milatul", "AXESS INTENSE FACTORY RACING", "INA", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208942270000), "PARTON Mikayla", "", "GBR", new Date(852076800000)]
+                }, {
+                    "columns": [new Date(-2208942240000), "ROA SANCHEZ Valentina", "MS-RACING", "COL", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208942210000), "MILLS Sacha", "", "AUS", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208942180000), "EMPEY Elise", "CONTINENTAL ATHERTON", "AUS", new Date(1072915200000)]
+                }, {
+                    "columns": [new Date(-2208942150000), "BERNARD Mathilde", "", "FRA", new Date(946684800000)]
+                }, {
+                    "columns": [new Date(-2208942120000), "BONNAURE Lais", "", "FRA", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208942090000), "HOGIE Abigail", "", "USA", new Date(883612800000)]
+                }, {
+                    "columns": [new Date(-2208942060000), "GRANDOUILLER Emy", "", "FRA", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208942030000), "RØNNING Frida Helena", "", "NOR", new Date(788918400000)]
+                }, {
+                    "columns": [new Date(-2208942000000), "BESSON Soline", "", "FRA", new Date(1136073600000)]
+                }, {
+                    "columns": [new Date(-2208941970000), "FARINA Eleonora", "MS-RACING", "ITA", new Date(631152000000)]
+                }, {
+                    "columns": [new Date(-2208941940000), "CARRICK-ANDERSON Emily", "", "GBR", new Date(1104537600000)]
+                }, {
+                    "columns": [new Date(-2208941910000), "MCCULLY Bethany", "", "GBR", new Date(1072915200000)]
+                }]
