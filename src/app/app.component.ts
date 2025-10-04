810cae3f529557863dc2494f064354feea1020d3
p.olteanu <p.olteanu@rosoftlab.net>
2025-10-05 02:18:45 +0300

    new round...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -147,7 +147,7 @@ export class AppComponent implements OnInit {
 
       if (!localStorageBestTeams) {
         let start = performance.now();
-        this.bestTeams = this.findBestTeamsOptimized(this.data, this.money, 9, 38);
+        this.bestTeams = this.findBestTeamsOptimized(this.data, this.money, 10, 31);
         window.localStorage.setItem('bestTeams', JSON.stringify(this.bestTeams));
         let end = performance.now();
         alert(`Best teams calculated in ${(end - start).toFixed(2)} ms. Found ${this.bestTeams.length} best teams.`);
@@ -523,7 +523,7 @@ export class AppComponent implements OnInit {
 
   // find best teams
 
-  findBestTeamsOptimized(athletes: any[], budget = 1500000, roundsPlayed = 9, shortlistSize: number) {
+  findBestTeamsOptimized(athletes: any[], budget = 1500000, roundsPlayed = 10, shortlistSize: number) {
     const males = athletes.filter(a => a.gender === "Male");
     const females = athletes.filter(a => a.gender === "Female");
 
@@ -554,7 +554,6 @@ export class AppComponent implements OnInit {
       const withoutFirst = getCombinations(rest, k);
       return [...withFirst, ...withoutFirst];
     }
-    debugger;
     const results = [];
 
     for (let round = 1; round <= roundsPlayed; round++) {
